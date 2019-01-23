b9a708712d63654b64c472019975d271c58b55b4
山木隆寛 <yamaki.takahiro@gmail.com>
2019-01-23 23:40:13 +0800

    Fix warnings

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -18,7 +18,7 @@ class Track extends React.Component {
           <h3>name</h3> 
           <p>artistsName | albumName</p>
         </div>
-        <a href="*" className="Track-action"><!-- + or - will go here --> renderAction() </a>
+        <a href="*" className="Track-action">- + or - will go here --> renderAction() </a>
       </div>          
     );
   }

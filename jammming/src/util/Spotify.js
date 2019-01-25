4ddcaf8973545c1923dbb454c19c15606117de90
山木隆寛 <yamaki.takahiro@gmail.com>
2019-01-25 20:20:11 +0800

    delete log

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -1,7 +1,5 @@
 const clientID = process.env.REACT_APP_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-console.log(`clientID: ${clientID}`);
-console.log(`clientSecret: ${clientSecret}`);
 
 const Spotify = {
   search (term) {
